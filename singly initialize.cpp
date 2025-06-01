65a4c5e543ba07a38ec76883a01fa298a171b150
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-06-01 14:18:15 +0530

    Add files via upload

---
+++ singly initialize.cpp
@@ -0,0 +1,41 @@
+
+//method 1 - define and create one node with values
+//using constructor to initialize(better)
+#include <iostream>
+using namespace std;
+struct Node{
+	public:
+	int data;
+	Node* next;
+	Node(int data1,Node* next1){
+		data=data1;
+		next=next1;
+		
+	}
+	
+};
+
+int main(){
+	Node *head=new Node(4,nullptr);
+	cout<<"Data: "<<head->data<<endl;
+	return 0;
+}
+
+
+//method2 (not a good choice) direct in main
+#include <iostream>
+using namespace std;
+
+struct Node{
+	public:
+	int data;
+	Node* next;	
+};
+int main(){
+	Node *head=new Node();  //default constructor will work here
+	head->data=4;
+	head->next=nullptr;
+	
+	cout<<"Node: "<<head->data<<endl;
+	return 0;
+}	
